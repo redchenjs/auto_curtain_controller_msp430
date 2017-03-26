f5fb111c69c6adcffcfe61515597873a92026b2e
redchenjs <redchenjs@live.com>
2017-03-26 15:31:20 +0800

    fix motor problem: wrong status indication

--- src/user/motor.c
+++ src/user/motor.c
@@ -17,27 +17,19 @@ unsigned char motor_status_past = CLOSED;
 void motor_update(void)
 {
     motor_status_past = motor_status_now;
-    if (stepper_ready) {
-        switch (motor_status_now) {
-            case CLOSED:
-            case OPENED:
-                if (stepper_location_now > stepper_location_set) {
-                    motor_status_now = OPENING;
-                    stepper_update();
-                }
-                else if (stepper_location_now < stepper_location_set) {
-                    motor_status_now = CLOSING;
-                    stepper_update();
-                }
-                break;
-            case CLOSING:
-            case OPENING:
-                motor_status_now -= 2;
-                record_write_all();
-                break;
-            default:
-                break;
-        }
+
+    if (stepper_location_now > stepper_location_set) {
+        motor_status_now = OPENING;
+        stepper_update();
+    }
+    else if (stepper_location_now < stepper_location_set) {
+        motor_status_now = CLOSING;
+        stepper_update();
+    }
+
+    if (stepper_ready && (motor_status_now == CLOSING || motor_status_now == OPENING)) {
+        motor_status_now -= 2;
+        record_write_all();
 	}
 }
 
